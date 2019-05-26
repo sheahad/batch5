f32b88b20eee4e9038fa307ffc197fe7a2daf294
unknown <sheahad@gmail.com>
2019-05-26 22:51:26 +0600

    U1_List_Label_added

--- MyWinApp/MyWinApp/ListForm.Designer.cs
+++ MyWinApp/MyWinApp/ListForm.Designer.cs
@@ -32,11 +32,13 @@
             this.showRichTextBox = new System.Windows.Forms.RichTextBox();
             this.numberTextBox = new System.Windows.Forms.TextBox();
             this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(122, 74);
+            this.AddButton.Location = new System.Drawing.Point(206, 74);
             this.AddButton.Name = "AddButton";
             this.AddButton.Size = new System.Drawing.Size(75, 23);
             this.AddButton.TabIndex = 0;
@@ -54,23 +56,43 @@
             // 
             // numberTextBox
             // 
-            this.numberTextBox.Location = new System.Drawing.Point(16, 33);
+            this.numberTextBox.Location = new System.Drawing.Point(97, 48);
             this.numberTextBox.Name = "numberTextBox";
             this.numberTextBox.Size = new System.Drawing.Size(100, 20);
             this.numberTextBox.TabIndex = 2;
             // 
             // nameTextBox
             // 
-            this.nameTextBox.Location = new System.Drawing.Point(16, 74);
+            this.nameTextBox.Location = new System.Drawing.Point(97, 74);
             this.nameTextBox.Name = "nameTextBox";
             this.nameTextBox.Size = new System.Drawing.Size(100, 20);
             this.nameTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Number";
             // 
             // ListForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(303, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.nameTextBox);
             this.Controls.Add(this.numberTextBox);
             this.Controls.Add(this.showRichTextBox);
@@ -88,5 +110,7 @@
         private System.Windows.Forms.RichTextBox showRichTextBox;
         private System.Windows.Forms.TextBox numberTextBox;
         private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
